4b1bc646fa42946d6374458c539691464ff5fa18
JustinChilton <JustinChilton@users.noreply.github.com>
2020-11-13 16:42:44 +0000

    Automated deployment: Fri Nov 13 16:42:44 UTC 2020 508c45f51fe9ded1f611f70e93f48319ef6dd39f

---
+++ classes/JSL/UtCompositeRunReporter-Summary.js
@@ -0,0 +1,1 @@
+NDFramePage.OnPageTitleLoaded("JSLClass:UtCompositeRunReporter","UtCompositeRunReporter");NDSummary.OnSummaryLoaded("JSLClass:UtCompositeRunReporter",[["JSL","JSL"]],[["Classes","Class"],["Functions","Function"],["Groups","Group"]],[[221,0,0,"UtCompositeRunReporter"],[222,0,2,"Functions","Functions"],[223,0,1,"ut composite run reporter","ut_composite_run_reporter"]]);
